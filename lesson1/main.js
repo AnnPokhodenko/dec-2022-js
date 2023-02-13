1b3ac36f27aadb4cd6c6126c4a77c30bda0a4eff
AnnPokhodenko <anne.pokhodenko@gmail.com>
2023-02-13 17:28:10 +0200

    Homework_done

---
+++ lesson1/main.js
@@ -0,0 +1,56 @@
+let str = 'hello';
+console.log(str)
+
+let str1 = 'owu';
+console.log(str1)
+
+let str2 = 'com';
+console.log(str2)
+
+let str3 = 'ua';
+console.log(str3)
+
+let num = 1;
+console.log(num)
+
+num = 10;
+console.log(num)
+
+num = -999;
+console.log(num)
+
+num = 123;
+console.log(num)
+
+num = 3.14;
+console.log(num)
+
+num = 2.7
+console.log(num)
+
+num = 16;
+console.log(num)
+
+let bool = true;
+console.log(bool)
+
+let bool1 = false;
+console.log(bool1)
+
+
+
+let lastName = 'Походенко'
+let firstName = 'Анна'
+let middleName = 'Ігорівна'
+console.log(`${lastName} ${firstName} ${middleName}`)
+
+
+
+let a = 100;
+console.log(typeof a)
+
+let b = '100';
+console.log(typeof b)
+
+let c = true;
+console.log(typeof c)
